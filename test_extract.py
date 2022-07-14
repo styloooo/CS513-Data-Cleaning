05fc31d3a64ff33808202ad42b59a7f1f201e852
Tyler Davis <tallyndavis@comcast.net>
2022-07-14 14:34:51 -0400

    outlined additional test cases

--- test_extract.py
+++ test_extract.py
@@ -1,12 +1,45 @@
 import unittest
-from extract_helper import read_csv
-from utils import DATA_FILE_PATH
 
-class ExtractTests(unittest.TestCase):
-    
+from os import path, remove
+
+from extract_helper import read_csv, write_csv
+from utils import DATA_FILE_PATH, TEST_DATA_DIR
+
+class CsvTests(unittest.TestCase):
+
+    write_path = path.join(TEST_DATA_DIR, 'test_write_csv.csv')
+
+    @classmethod
+    def tearDownClass(cls):
+        remove(cls.write_path)
+
     def test_read_csv(self):
         rows = read_csv(DATA_FILE_PATH)
         self.assertEquals(len(rows), 129972 - 1)  # excludes header
+
+    def test_write_csv(self):
+        rows = [
+            [1, 2, 3, 4],
+            ['A', 'B', 'C', 'D'],
+            ['foo', 'bar', 'spam', 'eggs']
+        ]
+
+        write_csv(type(self).write_path, rows)
+        # self.assert
+
+class TextProcessingTests(unittest.TestCase):
+    
+    def test_stemmer(self):
+        pass
+
+    def test_stopword_detection(self):
+        pass
+
+class WordIdTests(unittest.TestCase):
+
+    # Testing generated Word ID functional dependency (PKs)
+    def test_word_id_fd(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
