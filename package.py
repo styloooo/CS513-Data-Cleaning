21d57505b1a610c0e804abbed8f12dd7524affe2
Tyler Davis <tallyndavis@comcast.net>
2022-07-20 13:12:19 -0400

    extract script now copies processed data files

--- package.py
+++ package.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+from utils import (DATA_FILE_PATH, DATA_FILE_NAME,
+                   WORDS_FILE_NAME, WORDS_FILE_PATH,
+                   REVIEW_WORDS_FILE_NAME, REVIEW_WORDS_FILE_PATH)
 
 BASE_DIR = os.getcwd()
 FILE_NAMES = (
@@ -17,6 +20,14 @@ FILE_NAMES = (
 
 FILE_PATHS = {os.path.join(BASE_DIR, fName): fName for fName in FILE_NAMES}
 OUTPUT_DIR = os.path.join(BASE_DIR, 'text-extract-packaged')
+OUTPUT_DATA_DIR = os.path.join(OUTPUT_DIR, 'data')
+INPUT_DATA_FILE_PATH = os.path.join(OUTPUT_DATA_DIR, DATA_FILE_NAME)
+
+DATA_FILE_PATHS = {
+    DATA_FILE_PATH: DATA_FILE_NAME,
+    WORDS_FILE_PATH: WORDS_FILE_NAME,
+    REVIEW_WORDS_FILE_PATH: REVIEW_WORDS_FILE_NAME
+}
 
 def main():
     if not os.path.exists(OUTPUT_DIR):
@@ -25,8 +36,18 @@ def main():
         shutil.rmtree(OUTPUT_DIR)
         os.mkdir(OUTPUT_DIR)
 
+    if not os.path.exists(OUTPUT_DATA_DIR):
+        os.makedirs(OUTPUT_DATA_DIR)
+    else:
+        shutil.rmtree(OUTPUT_DATA_DIR)
+        os.makedirs(OUTPUT_DATA_DIR)
+
     for path, fileName in FILE_PATHS.items():
         output_path = os.path.join(OUTPUT_DIR, fileName)
+        shutil.copy(path, output_path)
+
+    for path, fileName in DATA_FILE_PATHS.items():
+        output_path = os.path.join(OUTPUT_DATA_DIR, fileName)
         shutil.copy(path, output_path)
 
 if __name__ == '__main__':
